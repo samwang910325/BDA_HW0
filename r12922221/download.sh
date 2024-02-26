9fc897c61046307c3f862e3871eb2f1f6e3e93b6
samwang910325 <73706005+samwang910325@users.noreply.github.com>
2024-02-26 14:35:35 +0800

    Update download.sh

--- r12922221/download.sh
+++ r12922221/download.sh
@@ -1,1 +1,3 @@
 mkdir videos
+wget 'https://docs.google.com/uc?export=download&id=1F2zPCLVyTZ89clza5Rk9EKZwK4zCGOQd' -O videos/line.mp4
+wget 'https://docs.google.com/uc?export=download&id=1AvHQsANno4Myg2H5NgPICDiqW4em6M1Y' -O videos/spotify.mp4
